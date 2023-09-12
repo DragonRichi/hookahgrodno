17021ef8c7350ff4841419a07b03c2618354b789
DragonRichi <lucasrichi123@gmail.com>
2023-09-12 11:38:53 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
 const form = document.querySelector(".form-feedback")
 const formButton = document.querySelector(".form-feedback-button")
 
+
 const inputName = document.querySelector("#inputName")
 const inputTel = document.querySelector("#inputTel")
+
+const burger = document.querySelector(".menu__list.active");
+const burgerID = document.querySelector("#hamb");
+const hamb = document.querySelector("#hamb");
+const menu = document.querySelector("#menu__list");
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler);
+Burger();
+
+function hambHandler(e) {
+  e.preventDefault();
+  menu.classList.toggle("active");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+}
+function Burger() {
+  document.addEventListener("click", (e) => {
+    const click9 = e.composedPath().includes(burgerID);
+    const click10 = e.composedPath().includes(burger);
+    if (!click9 && !click10) {
+      menu.classList.remove("active");
+      hamb.classList.remove("active");
+      body.classList.remove("noscroll");
+    }
+  });
+}
+
 formButton.addEventListener("click", (e) => {
   e.preventDefault()
   if (inputTel.value.length <= 6) {
